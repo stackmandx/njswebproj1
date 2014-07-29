b6b79c57a3b3c401106b558e6c822484e77d579d
Joey Chan <ppi326@hotmail.com>
2014-07-29 14:57:09 -0700

    carousel and navbar skeleton

--- colori/app.js
+++ colori/app.js
@@ -15,7 +15,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine("dot", require("dot-emc").init({app: app, fileExtension:"dot"}).__express);
 app.set("view engine", "dot");
-app.use(express.favicon(path.join(__dirname, '/icon.ico')));
+//app.use(express.favicon(path.join(__dirname, '/icon.ico')));
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
