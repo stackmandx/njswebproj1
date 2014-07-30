044cb944f5e0c856961b11278fff0419b4bdef20
Joey Chan <ppi326@hotmail.com>
2014-07-30 15:04:58 -0700

    some changes bla bla

--- colori/routes/index.js
+++ colori/routes/index.js
@@ -3,6 +3,27 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
+exports.index = function (req, res, next) {
+    
   res.render('index', { title: 'Colori Kitchen' });
 };
+
+exports.menu = function (req, res) {
+    res.render('index', {});
+};
+exports.catering = function (req, res){
+    res.render('index', {});
+};
+exports.gallery = function (req, res){
+    res.render('index', {});
+};
+exports.reserve = function (req, res){
+    res.render('index', {});
+};
+exports.luigi = function (req, res){
+    res.render('index', {});
+};
+exports.contact = function (req, res){
+    res.render('index', {});
+};
+
