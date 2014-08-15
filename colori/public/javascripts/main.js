bbe152ef93fd42d6c754672c50ef1d6fb2783761
Joey Chan <ppi326@hotmail.com>
2014-08-15 14:57:57 -0700

    working on about page

--- colori/public/javascripts/main.js
+++ colori/public/javascripts/main.js
@@ -1,6 +1,16 @@
 ﻿$(function () {
-    $('#carousel').carousel();
+    $('#carousel').carousel('pause');
     $('.navbar ul li a').click(function(){
     	return false;
     })
+
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 30) {
+            $(".navbar-fixed-top").addClass("sticky");
+        }
+        else {
+            $(".navbar-fixed-top").removeClass("sticky");
+        }
+    });
+
 });
